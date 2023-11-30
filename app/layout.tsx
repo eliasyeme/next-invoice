9fdb2d87cf3399a59007cb12b9cd741b1f29afce
eliasyeme <eliasyeme@gmail.com>
2023-11-30 12:36:51 +0300

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import '@/app/ui/global.css';
+import { ReactNode } from 'react';
+import { inter } from './ui/fonts';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/invoices/status.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+
+export default function InvoiceStatus({
+  status,
+}: {
+  status: 'pending' | 'paid';
+}) {
+  return (
+    <span
+      className={clsx(
+        'inline-flex items-center rounded-full px-2 py-1 text-sm',
+        {
+          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-green-500 text-white': status === 'paid',
+        },
+      )}
+    >
+      status
+    </span>
+  );
+}
