e46b7e68df1857b8f3263fdf9dbd2e7a1cd3c6f2
eliasyeme <eliasyeme@gmail.com>
2023-12-01 10:00:37 +0300

    add recent revenue and latest invoice components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <p>Dashboard Page</p>;
+import { lusitan } from "@/app/ui/fonts";
+import { fetchRevenue, fetchLatestInvoices } from "@/app/lib/data";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart"
+import LatestInvoice from "@/app/ui/dashboard/latest-invoice";
+
+export default async function Page() {
+	const revenue = await fetchRevenue();
+	const latestInvoice = await fetchLatestInvoices();
+
+  return (
+		<main>
+			<h1 className={`${lusitan.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+			<div>
+				{/* TODO: card */}
+			</div>
+			<div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+				<RevenueChart revenue={revenue}/>
+				<LatestInvoice latestInvoice={latestInvoice}/>
+			</div>
+		</main>
+	)
 }
